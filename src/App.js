7107a5f58c0cdcb56ebabaf9328605064df759c9
Landon West <landonwest5@gmail.com>
2020-05-06 16:16:30 -0400

    update project name; fix typo; add and render message from state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [myGreeting, setMyGreeting] = useState();
+
+  useEffect(() => {
+    fetch('/v1/api').then(res => res.json()).then(data => {
+      setMyGreeting(data.greeting);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h2>{myGreeting}</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
