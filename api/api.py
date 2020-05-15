40c19fa96b2fa2ee45f488ea7a745ceb843d8835
Landon West <landonwest5@gmail.com>
2020-05-15 02:17:02 -0400

    rename, rearrange; add flask_migrate

--- api/api.py
+++
@@ -1,8 +0,0 @@
-from api import app, db
-from api.models.user import User
-from api.routes.routes import *
-
-
-with app.test_request_context():
-     db.init_app(app)
-     db.create_all()
---
+++ api/app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database Config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Initialize Flask plugins
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+migrate = Migrate(app, db)
+# flask db migrate -m "migration message"
+# flask db upgrade
+
+# Run server
+# (this is an older way of doing it and `flask run` from root dir is preferred)
+# if __name__ == ("__main__"):
+#     app.run()
